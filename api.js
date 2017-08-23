f0a1cc92ee13cc0f81c100e14e1a72963515d3f5
Prem Prakash <premprakash096@gmail.com>
2017-08-23 11:43:08 +0530

    minor structural changes.

--- api.js
+++ api.js
@@ -4,16 +4,20 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
 //var affiliateToken = "ea351af4dbbb4c71bd889b2527b3f883"
-var affiliateId = "premprakash096"
+var affiliateId = 'premprakash096'
+var affiliateToken = '671645dfd6d1422692963bd673804771'
 var request = require('request');
-
+let productCollection = "product"
        
+
 
 var exportData = function(app) {
 	createDB(app);
 	createCollection(app);
 	refreshData(app);
 	getAllCategories(app);
+	fetchCategoryData(app);
+	createProductCollection(app);
 }
 
   var createDB =  function (app){ 
@@ -46,7 +50,6 @@ var exportData = function(app) {
 	res.send(req.query.collection + " collection created!");
 })
 }
-
 
 
 
@@ -101,6 +104,87 @@ var getAllCategories = function(app){
 	  
 	  })
 }
+
+var createProductCollection = function(app) {
+	app.get('/createProductCollection',function(req,res) {
+
+		MongoClient.connect(url, function(err, db) {
+
+db.listCollections({name: productCollection})
+    .next(function(err, collinfo) {
+        if (collinfo) {
+            console.log("Collection found");
+            res.send(productCollection + " collection created!")
+        }
+        else {
+        db.createCollection(productCollection, function(err, res) {
+		    if (err) throw err;
+		    console.log(productCollection + " collection created!");
+		    res.send(productCollection + " collection created!")
+		    
+		  });
+        	
+        }
+    });
+
+	})
+})
+}
+
+var fetchCategoryData = function(app){ 
+ 	app.get('/fetch',function(req,res) {
+		
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+
+
+	  	var collection = db.collection("categories");	
+	  	let categoryName = req.query.collection;
+	  	  
+		collection.find({resourceName:categoryName},{"get":1}).toArray(function(err, items) {
+         let url = items[0].get;
+
+
+		var options = { method: 'GET',
+  						url: url, 
+  						 headers:  { 
+					     'fk-affiliate-token': affiliateToken,
+     					 'fk-affiliate-id': affiliateId } 
+     				};
+
+		request(options, function (error, response, body) {
+  		if (error) throw new Error(error);
+
+	    console.log(error);
+	    console.log(response);
+	    console.log(body);
+	  	var obj = JSON.parse(body);
+		var objects = []
+
+		var count = 0;
+	  	for (var i in obj.productInfoList) {
+	  		var data = obj.productInfoList[i];
+	  		console.log(data)
+	  		objects.push(data)
+		  	}
+		  		  		
+		  	db.collection(productCollection).insertMany(objects, function(err, res) {
+		    if (err) throw err;
+		    
+		  	});
+		  	db.close();
+		
+
+  			console.log(body);
+		});
+         res.send(url);
+		});
+	  
+	  })
+})
+}
+
+
 
 
 
--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var app = express()
 app.use(cors())
 
 
-var node1 = require("./index1.js")(app);
-var node2 = require("./index2.js");
+var apiNode = require("./api.js");
 //var node3 = require("./index2.js")(app1);
 // node2.abcd(app)
 // node2.abcde(app)
-node2.exportData(app)
+apiNode.exportData(app)
 
 //console.log(node2.abcde)
 app.listen(8080);
